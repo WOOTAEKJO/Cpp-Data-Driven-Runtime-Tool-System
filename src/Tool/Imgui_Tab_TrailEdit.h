78f165bb8a76beb2df5989504599215dc9f834cc
WOOTAEKJO <114208566+WOOTAEKJO@users.noreply.github.com>
2026-08-19 18:16:17 +0900

    Add CImgui_Tab_TrailEdit class for trail editing

---
+++ src/Tool/Imgui_Tab_TrailEdit.h
@@ -0,0 +1,72 @@
+#ifdef _DEBUG
+#pragma once
+#include "Imgui_Tab_EffectTabBase.h"
+#include "Trail_Effect.h"
+ 
+BEGIN(Client)
+
+class CImgui_Tab_TrailEdit : public CImgui_Tab_EffectTabBase
+{
+
+private:
+	CImgui_Tab_TrailEdit(vector< shared_ptr<CGameObject>>* pGameObjectList, ComPtr<ID3D11Device> pDevice, ComPtr<ID3D11DeviceContext> pContext);
+	virtual	~CImgui_Tab_TrailEdit() = default;
+
+public:
+	virtual HRESULT Initialize();
+	virtual void Tick(_cref_time fTimeDelta);
+	virtual HRESULT Render();
+
+public:
+	shared_ptr<CTrail_Effect> m_pTrailEffect = { nullptr };
+private:
+	CTrail_Effect::TRAIL_DESC m_TrailDesc;
+
+	/*vector<wstring> m_PrototypeModelList;
+	vector<wstring> m_PrototypeDiffuseList;
+	vector<wstring> m_PrototypeMaskList;
+	vector<wstring> m_PrototypeNoiseList;
+	vector<wstring> m_PrototypeDissolveList;*/
+
+	//_int m_iPickIndex_PrtTag[5] = { 0,0,0,0,0 };
+
+	/*_int	m_iTexTypeRadio = { 0 };
+	_int	m_iUseTypeRadio = { 0 };
+
+	_int	m_iNumLoad = { 0 };*/
+
+private:
+	_int m_iParticleType = { 0 };
+
+	wstring m_strModelTag = {};
+	// ----- DeadType -------
+
+	_int	m_iDeadType = { 0 };
+
+private:
+	_int    m_iSelectedEffect_Index = { -1 };
+
+public:
+	static CImgui_Tab_TrailEdit* Create(vector< shared_ptr<CGameObject>>* pGameObjectList, ComPtr<ID3D11Device> pDevice, ComPtr<ID3D11DeviceContext> pContext);
+	virtual void	Free() override;
+
+#pragma region 그룹
+
+public: // 수정전용
+	void	Set_Effect(shared_ptr<CTrail_Effect> pEffect);
+	void	Remove_Effect();
+
+//private:
+//	_bool	m_bModify = { false }; // 수정전용
+//	_bool	m_bLoad = { false };
+//	string	m_strLoadFilePath;
+
+#pragma endregion
+
+
+
+};
+
+END
+
+#endif // DEBUG
